477105a3112620df5ea2cbbb9a446098f0a80473
Rob Nelson <rnelson0@gmail.com>
2014-05-27 21:19:25 +0000

    Update fact with default role for unknown hostnames

--- lib/facter/roles.rb
+++ lib/facter/roles.rb
@@ -3,11 +3,16 @@ if Facter.value(:hostname) =~ /^([a-z]+)[0-9]+$/
   Facter.add('puppet_role') do
     setcode {$1}
   end
-end
 
 # ([a-z]+), i.e. www or logger have a puppet_role of www or logger
-if Facter.value(:hostname) =~ /^([a-z]+)$/
+elsif Facter.value(:hostname) =~ /^([a-z]+)$/
   Facter.add('puppet_role') do
     setcode {$1}
+  end
+
+# Set to hostname if no patterns match
+else
+  Facter.add('puppet_role') do
+    setcode 'default'
   end
 end
